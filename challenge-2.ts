6e16af71f855a53890f0c8ff3f3712f99d68b50a
Abdallah Daknache <58996641+abdallahdak7@users.noreply.github.com>
2023-01-15 14:27:49 +0200

    Update challenge-2.ts

--- challenge-2.ts
+++ challenge-2.ts
@@ -3,7 +3,6 @@ function pivotIndex(nums: number[]): number {
     let rightSum = nums.reduce((acc, curr) => acc + curr);
     for (let i = 0; i < nums.length; i++) {
         rightSum -= nums[i];
-        console.log(rightSum + ' === ' + leftSum)
         if (leftSum === rightSum) {
             return i;
         }
