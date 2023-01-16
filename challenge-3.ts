f9d608c3893f465a90f46b9ec9680b07b9cd9d1a
Abdallah Daknache <58996641+abdallahdak7@users.noreply.github.com>
2023-01-16 22:29:41 +0200

    Create challenge-3.ts

---
+++ challenge-3.ts
@@ -0,0 +1,18 @@
+function isIsomorphic(s: string, t: string): boolean {
+    if (s.length !== t.length) {
+        return false;
+    }
+
+    let map = new Map<string, string>();
+    for (let i = 0; i < s.length; i++) {
+        if (!map.has(s[i])) {
+            if (Array.from(map.values()).includes(t[i])) {
+                return false;
+            }
+            map.set(s[i], t[i]);
+        } else if (map.get(s[i]) !== t[i]) {
+            return false;
+        }
+    }
+    return true;
+}
