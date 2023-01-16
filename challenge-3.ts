5056f37afc5275c5516c9cddcccbee905eab9b55
Abdallah Daknache <58996641+abdallahdak7@users.noreply.github.com>
2023-01-16 23:01:42 +0200

    Update challenge-3.ts

--- challenge-3.ts
+++ challenge-3.ts
@@ -17,6 +17,16 @@ function isIsomorphic(s: string, t: string): boolean {
     return true;
 }
 
+function isIsomorphic(s, t) {
+  if (s.length != t.length) return false;
+
+  for (let i = 1; i < s.length; i++) {
+    if (t.indexOf(t[i]) !== s.indexOf(s[i])) return false;
+  }
+
+  return true;
+}
+
 /*
 Input: s = "egg", t = "add"
 Output: true
