9d92230579affaf771a50487e99a3f1dc50b7ace
Abdallah Daknache <58996641+abdallahdak7@users.noreply.github.com>
2023-01-17 17:56:43 +0200

    Create challenge-4.ts

---
+++ challenge-4.ts
@@ -0,0 +1,22 @@
+function isSubsequence(s: string, t: string): boolean {
+    let i = 0;
+    let j = 0;
+    while (i < s.length && j < t.length) {
+        if (s[i] === t[j]) {
+            i++;
+        }
+        j++;
+    }
+    return i === s.length;
+}
+
+/*
+Example 1:
+
+Input: s = "abc", t = "ahbgdc"
+Output: true
+Example 2:
+
+Input: s = "axc", t = "ahbgdc"
+Output: false
+**/
